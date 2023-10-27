4a1136b9c47722fc00c54dc932d9a7ef457c7f5a
luna <luna@smluna.lnl.infn.it>
2023-10-27 15:43:30 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir build && cd build 
-cmake ../AZURE2/ -DBUILD_GUI=ON -DUSE_QWT=ON && make -j4 && cd -
+cmake .. -DBUILD_GUI=ON -DUSE_QWT=ON && make -j4 && cd -
 
 mkdir -p $PREFIX/bin
 cp build/src/AZURE2 $PREFIX/bin/
